10f8de429875f4cba1bc5f254b66d9296ec7cc1a
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2025-03-06 17:47:57 +0100

    Add aruco markers detector
    Aruco detector node subscribing to camera frames
    and publishing aruco markers ids and corners
    
    TODO: pose estimation of markers

---
+++ src/drone_detector/drone_detector/aruco_detector.py
@@ -0,0 +1,68 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from drone_interfaces.msg import ArucoMarker, ArucoMarkersList
+from cv_bridge import CvBridge
+import cv2
+import cv2.aruco as aruco
+
+
+class ArucoDetector(Node):
+    def __init__(self):
+        super().__init__('aruco_detector')
+
+        # Create the subscriber. This subscriber will receive an Image
+        # from the video_frames topic. The queue size is 10 messages.
+        self.subscription = self.create_subscription(
+            Image,
+            'camera',
+            self.camera_callback,
+            10)
+
+        # Create the publisher for Aruco marker data
+        self.publisher = self.create_publisher(ArucoMarkersList, 'aruco_markers', 10)
+
+        self.br = CvBridge()
+        self.get_logger().info('aruco_detector node created')
+
+    def camera_callback(self, data):
+        current_frame = self.br.imgmsg_to_cv2(data)
+
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+
+        # Load the predefined dictionary
+        aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_100)
+
+        # Initialize the detector parameters using default values
+        parameters = aruco.DetectorParameters_create()
+
+        # Detect the markers in the image
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+
+        aruco_markers_list = ArucoMarkersList()
+        if ids is not None:
+            for i in range(len(ids)):
+                marker_id = ids[i][0]
+                marker_corners = corners[i].flatten().tolist()
+                aruco_marker = ArucoMarker()
+                aruco_marker.marker_id = int(marker_id)
+                aruco_marker.corners = marker_corners
+                aruco_markers_list.aruco_markers.append(aruco_marker)
+
+        self.publisher.publish(aruco_markers_list)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    aruco_detector = ArucoDetector()
+    rclpy.spin(aruco_detector)
+
+    aruco_detector.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- src/drone_detector/setup.py
+++ src/drone_detector/setup.py
@@ -38,7 +38,8 @@ setup(
             "detector=drone_detector.detector:main",
             "detection_subscriber=drone_detector.detection_subscriber:main",
             "detector_server=drone_detector.detector_server:main",
-            "detection_client=drone_detector.detection_client:main"
+            "detection_client=drone_detector.detection_client:main",
+            "aruco_detector=drone_detector.aruco_detector:main",
         ],
     },
 )
