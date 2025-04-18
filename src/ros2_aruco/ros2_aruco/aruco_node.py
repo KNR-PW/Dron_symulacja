ab2f458e6a0bd12454699890866fd06692fbb5e3
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2025-04-18 23:05:02 +0200

    Adjust ros2_aruco package to knr needs

--- src/ros2_aruco/ros2_aruco/aruco_node.py
+++ src/ros2_aruco/ros2_aruco/aruco_node.py
@@ -37,9 +37,8 @@ import tf_transformations
 from sensor_msgs.msg import CameraInfo
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import PoseArray, Pose
-from ros2_aruco_interfaces.msg import ArucoMarkers
+from drone_interfaces.msg import ArucoMarkers
 from rcl_interfaces.msg import ParameterDescriptor, ParameterType
-
 
 class ArucoNode(rclpy.node.Node):
     def __init__(self):
@@ -57,7 +56,8 @@ class ArucoNode(rclpy.node.Node):
 
         self.declare_parameter(
             name="aruco_dictionary_id",
-            value="DICT_5X5_250",
+            # value="DICT_5X5_250",
+            value="DICT_4X4_100",
             descriptor=ParameterDescriptor(
                 type=ParameterType.PARAMETER_STRING,
                 description="Dictionary that was used to generate markers.",
@@ -66,7 +66,7 @@ class ArucoNode(rclpy.node.Node):
 
         self.declare_parameter(
             name="image_topic",
-            value="/camera/image_raw",
+            value="/camera",
             descriptor=ParameterDescriptor(
                 type=ParameterType.PARAMETER_STRING,
                 description="Image topic to subscribe to.",
@@ -74,22 +74,23 @@ class ArucoNode(rclpy.node.Node):
         )
 
         self.declare_parameter(
-            name="camera_info_topic",
-            value="/camera/camera_info",
+            name="intrinsic_matrix",
+            value=[1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0],
             descriptor=ParameterDescriptor(
-                type=ParameterType.PARAMETER_STRING,
-                description="Camera info topic to subscribe to.",
+                type=ParameterType.PARAMETER_DOUBLE_ARRAY,
+                description="Camera intrinsic matrix.",
             ),
         )
 
         self.declare_parameter(
-            name="camera_frame",
-            value="",
+            name="distortion",
+            value=[0.0, 0.0, 0.0, 0.0],
             descriptor=ParameterDescriptor(
-                type=ParameterType.PARAMETER_STRING,
-                description="Camera optical frame to use.",
+                type=ParameterType.PARAMETER_DOUBLE_ARRAY,
+                description="Camera distortion coefficients.",
             ),
         )
+
 
         self.marker_size = (
             self.get_parameter("marker_size").get_parameter_value().double_value
@@ -106,14 +107,32 @@ class ArucoNode(rclpy.node.Node):
         )
         self.get_logger().info(f"Image topic: {image_topic}")
 
-        info_topic = (
-            self.get_parameter("camera_info_topic").get_parameter_value().string_value
+        self.intrinsic_mat = (
+            self.get_parameter("intrinsic_matrix").get_parameter_value().double_array_value
         )
-        self.get_logger().info(f"Image info topic: {info_topic}")
+        self.intrinsic_mat = np.reshape(np.array(self.intrinsic_mat), (3, 3))
+        self.get_logger().info(f"Intrinsic matrix: {self.intrinsic_mat}")
 
-        self.camera_frame = (
-            self.get_parameter("camera_frame").get_parameter_value().string_value
+        self.distortion = (
+            self.get_parameter("distortion").get_parameter_value().double_array_value
         )
+        self.distortion = np.array(self.distortion)
+        self.get_logger().info(f"Distortion: {self.distortion}")
+
+        # Check that the intrinsic matrix is 3x3
+        if self.intrinsic_mat.size != 9:
+            self.get_logger().error(
+                "Intrinsic matrix must be 3x3, got {} values".format(
+                    len(self.intrinsic_mat)
+                )
+            )
+            return
+        # Check that the distortion is 1x4
+        if self.distortion.size != 4:
+            self.get_logger().error(
+                "Distortion must be 1x4, got {} values".format(len(self.distortion))
+            )
+            return
 
         # Make sure we have a valid dictionary id:
         try:
@@ -128,10 +147,6 @@ class ArucoNode(rclpy.node.Node):
             self.get_logger().error("valid options: {}".format(options))
 
         # Set up subscriptions
-        self.info_sub = self.create_subscription(
-            CameraInfo, info_topic, self.info_callback, qos_profile_sensor_data
-        )
-
         self.create_subscription(
             Image, image_topic, self.image_callback, qos_profile_sensor_data
         )
@@ -140,36 +155,20 @@ class ArucoNode(rclpy.node.Node):
         self.poses_pub = self.create_publisher(PoseArray, "aruco_poses", 10)
         self.markers_pub = self.create_publisher(ArucoMarkers, "aruco_markers", 10)
 
-        # Set up fields for camera parameters
-        self.info_msg = None
-        self.intrinsic_mat = None
-        self.distortion = None
 
         self.aruco_dictionary = cv2.aruco.Dictionary_get(dictionary_id)
         self.aruco_parameters = cv2.aruco.DetectorParameters_create()
         self.bridge = CvBridge()
 
-    def info_callback(self, info_msg):
-        self.info_msg = info_msg
-        self.intrinsic_mat = np.reshape(np.array(self.info_msg.k), (3, 3))
-        self.distortion = np.array(self.info_msg.d)
-        # Assume that camera parameters will remain the same...
-        self.destroy_subscription(self.info_sub)
-
     def image_callback(self, img_msg):
-        if self.info_msg is None:
-            self.get_logger().warn("No camera info has been received!")
-            return
+        cv_image = self.bridge.imgmsg_to_cv2(img_msg)
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-        cv_image = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="mono8")
         markers = ArucoMarkers()
         pose_array = PoseArray()
-        if self.camera_frame == "":
-            markers.header.frame_id = self.info_msg.header.frame_id
-            pose_array.header.frame_id = self.info_msg.header.frame_id
-        else:
-            markers.header.frame_id = self.camera_frame
-            pose_array.header.frame_id = self.camera_frame
+
+        markers.header.frame_id = ""
+        pose_array.header.frame_id = ""
 
         markers.header.stamp = img_msg.header.stamp
         pose_array.header.stamp = img_msg.header.stamp
