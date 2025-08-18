b0f90e03e3bb052cf823578d5fc5cc67403651e6
Rivial104 <rivial104i300@gmail.com>
2025-08-18 15:53:42 +0200

    ogarniecie oaka na rpi

--- src/drone_autonomy/drone_autonomy/test_pawel_czernic.py
+++ src/drone_autonomy/drone_autonomy/test_pawel_czernic.py
@@ -5,22 +5,23 @@ import time
 class Mission(DroneController):
     def __init__(self):
         super().__init__()
+        
 
 def main(args=None):
     rclpy.init(args=args)
     mission = Mission()
     mission.arm()
-    mission.takeoff(10.0)
-    mission.send_goto_relative( 5.0, 0.0, 0.0)
-    mission.start_video()
-    time.sleep(5)
-    mission.send_goto_relative(0.0, 5.0, 0.0)
-    time.sleep(5)
-    mission.send_goto_relative(-5.0, 0.0, 0.0)
-    time.sleep(5)
-    mission.send_goto_relative(0.0, -5.0, 0.0)
-    time.sleep(5)
-    mission.stop_video()
+    mission.takeoff(4.0)
+    mission.send_goto_relative( 0.0, 0.0, 0.0)
+    mission.set_speed(0.5)
+    mission.send_goto_relative(0.0, 17.0,0.0)
+    for x in range(10):
+        mission.send_goto_relative(1.0, 0.0, 0.0)
+        mission.send_goto_relative(0.0, -16.0,0.0)
+        mission.send_goto_relative(1.0, 0.0, 0.0)
+        mission.send_goto_relative(0.0, 16.0,0.0)
+
+
     mission.land()
     mission.destroy_node()
     rclpy.shutdown()
