2eeb50f4d5b813480baaeb4f42e9baf25db09174
DieRust <lesiakrafi@gmail.com>
2025-10-21 19:02:56 +0200

    make support to px4 sim

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -1,14 +1,8 @@
-if [ "$(docker ps -a -q -f status=running -f name=knr_drone)" ]; then
-    if [ "$(docker ps -a -q -f status=running -f name=knr_drone_px4)" ]; then
-        echo "you have open 2 containers you should stop one to working with the scrpits"
-        exit 1
-    fi
-fi
 
-if [ "$(docker ps -a -q -f status=running -f name=knr_drone)" ]; then
+if docker ps --filter "status=running" --format '{{.Names}}' | grep -Fxq "knr_drone"; then
     ./dockerise_cmd.sh "cd ~/ros_ws && colcon build --packages-skip microxrcedds_agent px4_msgs px4_ros_com && source install/setup.bash && ros2 launch drone_bringup drone_simulation.launch.py"
-elif [ "$(docker ps -a -q -f status=running -f name=knr_drone_px4)" ]; then
-    echo "darkside"
+elif docker ps --filter "status=running" --format '{{.Names}}' | grep -Fxq "knr_drone_px4"; then
+        ./dockerise_cmd.sh "cd ~/ros_ws && colcon build && source install/setup.bash && MicroXRCEAgent udp4 -p 8888"
 else
     echo "you should start the docker container"
 fi
--- docker/dockerise_cmd.sh
+++ docker/dockerise_cmd.sh
@@ -5,21 +5,21 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-if [ "$(docker ps -a -q -f status=running -f name=knr_drone)" ]; then
-    if [ "$(docker ps -a -q -f status=running -f name=knr_drone_px4)" ]; then
-        echo "you have open 2 containers you should stop one to working with the scrpits"
+if docker ps --filter "status=running" --format '{{.Names}}' | grep -Fxq "knr_drone"; then
+    if docker ps --filter "status=running" --format '{{.Names}}' | grep -Fxq "knr_drone_px4"; then
+        echo "You have both 'knr_drone' and 'knr_drone_px4' running. Stop one before using the scripts."
         exit 1
     fi
 fi
 
-if [ "$(docker ps -a -q -f status=running -f name=knr_drone)" ]; then
+if docker ps --filter "status=running" --format '{{.Names}}' | grep -Fxq "knr_drone"; then
     echo "running command in knr_drone container: $1"
 
     docker exec -it knr_drone bash -c "source /opt/ros/humble/setup.bash && $1"
-elif [ "$(docker ps -a -q -f status=running -f name=knr_drone_px4)" ]; then
+elif docker ps --filter "status=running" --format '{{.Names}}' | grep -Fxq "knr_drone_px4"; then
     echo "running command in knr_drone_px4 container: $1"
 
-    docker exec -it knr_drone_px4 bash -c "source /opt/ros/humble/setup.bash && $1"
+    docker exec -it knr_drone_px4 bash -c "source /opt/ros/jazzy/setup.bash && $1"
 else
     echo "you should start the docker container"
 fi
