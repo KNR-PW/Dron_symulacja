8356707bad975ddd8b8165ec037a59c73937edf5
Rivial104 <rivial104i300@gmail.com>
2025-10-26 19:01:50 +0100

    połączenie launcha mamby i kamery

--- src/drone_bringup/launch/drone_mamba.launch.py
+++ src/drone_bringup/launch/drone_mamba.launch.py
@@ -1,50 +1,27 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
-import os
 
 def generate_launch_description():
-    #Ścieżka do launch file kamery DepthAI
-    depthai_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('depthai_ros_driver'),
-                'launch',
-                'rgbd_pcl.launch.py'
-            )
-        )
-    )
-
-    #Node od drona
-    drone_node = Node(
-        package='drone_hardware',
-        executable='drone_handler',
-    )
-
-    #Node do zapisu obrazu z kamery
-    recorder_node = Node(
-        package='drone_camera',
-        executable='images_recorder',
-        parameters=[
-            {'camera_topic': 'oak/rgb/image_raw'}
-        ]
-    )
-
-    # Node do detekcji ArUco
-    aruco_node = Node(
-        package='ros2_aruco',
-        executable='aruco_node',
-        parameters=[{
-            'image_topic': 'oak/rgb/image_raw'
-        }]
-    )
-
-    #wszystko
     return LaunchDescription([
-        depthai_launch,   # kamera
-        drone_node,       # sterowanie dronem
-        recorder_node,    # nagrywanie
-        aruco_node        # detekcja ArUco
+        Node(
+            package='drone_hardware',
+            executable='drone_handler',
+        ),
+        Node(
+            package='drone_camera',
+            executable='images_recorder',
+            parameters=[
+                {'camera_topic': 'oak/rgb/image_raw'}
+                # {'camera_topic': 'camera/image_raw'}
+                # {'camera_topic': 'camera'}
+            ]
+        ),
+        Node(
+            package='ros2_aruco',
+            executable='aruco_node',
+            parameters=[{
+                'image_topic': 'oak/rgb/image_raw'
+            }]
+        ),
+
     ])
