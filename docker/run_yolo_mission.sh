b82e02325de1a773a96e313f5ddea7cf0b6f4f14
Max326 <maxconnor326@gmail.com>
2025-11-10 16:33:03 +0100

    yolo following wip cherry picked

--- docker/run_yolo_mission.sh
+++ docker/run_yolo_mission.sh
@@ -1,1 +1,1 @@
-./dockerise_cmd.sh "source ~/ros_ws/install/setup.bash && ros2 run drone_autonomy follow_detections --ros-args -p image_width:=640 -p image_height:=480 -p detections_topic:=/detections -p target_alt:=2.0 -p kp:=0.6 -p deadband_px:=20 -p max_vel:=1.0 -p lowpass:=0.3 -p lost_timeout:=7.0"
+./dockerise_cmd.sh "source ~/ros_ws/install/setup.bash && ros2 run drone_autonomy follow_detections --ros-args -p image_width:=640 -p image_height:=480 -p detections_topic:=/detections -p target_alt:=2.0 -p kp:=0.6 -p deadband_px:=20 -p max_vel:=1.0 -p lowpass:=0.3 -p lost_timeout:=5.0"
