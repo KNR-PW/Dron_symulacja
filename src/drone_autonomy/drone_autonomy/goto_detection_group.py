61c53bc0b36a6034ab5d3fc8071f6457b0b01e67
KTokarekPW <hozazen@gmail.com>
2025-11-12 20:57:50 +0100

    Zmienilem Komentarz

--- src/drone_autonomy/drone_autonomy/goto_detection_group.py
+++ src/drone_autonomy/drone_autonomy/goto_detection_group.py
@@ -2,7 +2,7 @@ import rclpy  # Python Client Library for ROS 2
 from rclpy.node import Node  # Handles the creation of nodes
 from cv_bridge import CvBridge  # Package to convert between ROS and OpenCV Images
 import cv2  # OpenCV library
-import numpy as np  #np to znaczy duzo numpy to cos
+import numpy as np  #np to znaczy duzo numpy to cos nie wiem
 
 # from detection import Detection
 from drone_interfaces.msg import DetectionMsg, DetectionsList
