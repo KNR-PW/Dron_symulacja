c9da825723c41e076e5dd61f778efa5ccd1fa9d6
PawelKwiat8 <PawelKwiat8@users.noreply.github.com>
2026-02-10 22:18:10 +0100

    update

--- scripts/run_px4_vtol_sitl.sh
+++ scripts/run_px4_vtol_sitl.sh
@@ -1,2 +1,19 @@
+
+# Przykład: ./run_px4_vtol_sitl.sh small_city
+
+WORLD=${1:-aruco}
+
 cp -r ../knr_tiltrotor ../KNR_Drone_PX4_Autopilot/Tools/simulation/gz/models
-cd ../KNR_Drone_PX4_Autopilot && make px4_sitl gz_knr_tiltrotor_aruco
+cd ../KNR_Drone_PX4_Autopilot
+
+
+# Odśwież CMake tylko jeśli cel nie istnieje
+if ! ninja -C build/px4_sitl_default -t targets 2>/dev/null | grep -q "gz_knr_tiltrotor_$WORLD"; then
+    echo "Nowy świat - odświeżam CMake..."
+    touch src/modules/simulation/gz_bridge/CMakeLists.txt
+fi
+
+# Ustaw pozycję spawnu drona (x,y,z)
+export PX4_GZ_MODEL_POSE="0,0,4"
+
+make px4_sitl "gz_knr_tiltrotor_$WORLD"
