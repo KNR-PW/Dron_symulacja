215e1a52db15696c2359f273d3c6dfc3c256c95d
Konrad <hozazen@gmail.com>
2026-02-14 22:37:04 +0100

    SUAS mission added

--- src/drone_autonomy/drone_autonomy/SUAS_Multicopter.py
+++ src/drone_autonomy/drone_autonomy/SUAS_Multicopter.py
@@ -10,6 +10,7 @@ alt = 20.0  # Wysokosc na ktorej latamy
 
 meters = 25.0 # Odleglosc kwadratu w metrach i wzniosu, bo akurat pasuje dla 25m
 
+times = 20 # Liczba powtórzeń kwadratu, 1 mile = 1.6km więc 20 * 25m = 500m
 
 # 1° latitude ≈ 111320 m
 dlat = meters / 111320
@@ -24,7 +25,7 @@ def main(args=None):
     mission.takeoff(20.0)   # 50 feet = 15.24 meters, so we can use 20 meters for a safe takeoff margin
     time.sleep(5) # Wait for the drone to stabilize after takeoff
     mission._set_mode("MULTICOPTER")
-    for i in range(20): # 1 mile = 1.6km so 
+    for i in range(times): # 1 mile = 1.6km so 100*20 = 2km gives us some leeway for eventual GPS inaccuracies
         mission.send_goto_global(base_lat, base_lon, alt)
         time.sleep(1) # Wait for the drone to reach the waypoint
         mission.send_goto_global(base_lat, base_lon + dlon, alt)
