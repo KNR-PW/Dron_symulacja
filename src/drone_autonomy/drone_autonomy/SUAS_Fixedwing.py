b7076e0046db37bf92d228f3d18323079e6cf70c
olek1500 <olekpanas04@gmail.com>
2026-02-17 00:17:10 +0100

    Update mission to meet suas requirements

--- src/drone_autonomy/drone_autonomy/SUAS_Fixedwing.py
+++ src/drone_autonomy/drone_autonomy/SUAS_Fixedwing.py
@@ -1,24 +1,41 @@
 import rclpy
 import time
+import math
 from drone_comunication import DroneController
+base_lat = 47.398136
+base_lon = 8.549139
+alt = 20.0
+meters = 1000.0 
+
+times = 1 
+dlat = meters / 111320
+dlon = meters / (111320 * math.cos(math.radians(base_lat)))
 
 def main(args=None):
     rclpy.init(args=args)
     mission = DroneController()
     mission.arm()
-    mission.takeoff(20.0)   # 50 feet = 15.24 meters, so we can use 20 meters for a safe takeoff margin
-    time.sleep(5) # Wait for the drone to stabilize after takeoff
-    mission._set_mode("FIXED_WING")
-    for i in range(10): # 1 mile = 1.6km so this mission does 10 * 250m = 2.5km
-        mission.send_goto_global(47.398136, 8.549139, 10.0) # Move to the first waypoint
-        time.sleep(1) # Wait for the drone to reach the waypoint
-        mission.send_goto_global(47.397321, 8.546019, 10.0) # Move to the second waypoint
-        time.sleep(1) # Wait for the drone to reach the waypoint
+    mission.takeoff(20.0) # takeff in Quadcopter mode, so we can safely takeoff and stabilize before switching to fixed wing mode
+    time.sleep(4) # Wait for the drone to stabilize after takeoff
+    mission._set_mode("FIXED_WING") 
+    for i in range(times):  # 1 mile = 1.6km 4*1000 = 4000 so gives us some leeway for eventual GPS inaccuracies
+        mission.send_goto_global(base_lat, base_lon, alt)
+        mission.send_goto_global(base_lat, base_lon + dlon, alt)
+        mission.send_goto_global(base_lat + dlat, base_lon + dlon, alt)
+        mission.send_goto_global(base_lat + dlat, base_lon, alt)
+    mission.send_goto_global(base_lat + 0*dlat, base_lon, 50.0)
+    time.sleep(1)
+    mission.send_goto_global(base_lat + 1*dlat, base_lon, 70.0) 
+    time.sleep(1)
+    mission.send_goto_global(base_lat + 2*dlat, base_lon, 50.0) 
+    time.sleep(1)
+    mission.send_goto_global(base_lat, base_lon, 30.0) 
+    time.sleep(1)
+    mission._set_mode("MULTICOPTER") #change back to multicopter mode to safely land
+    time.sleep(5) 
     mission.land()
-    # mission.rtl()
     mission.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == "__main__":
     main()
